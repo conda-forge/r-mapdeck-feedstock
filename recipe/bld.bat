6ddeef2c9a202548e287800e38520fdd5a5d6011
Mervin Fansler <mmfansler@gmail.com>
2024-07-11 19:35:25 +0200

    use sed

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 REM inject C++ standard
-echo CXX_STD=CXX14 >> src/Makevars.win
+sed -i "/CXX_STD = CXX11/c\CXX_STD = CXX14" src\Makevars.win
 
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
